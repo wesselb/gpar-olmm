e5de2d09976f2e95e74fd3ce7df17fe925f2cfd0
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-01-05 17:03:04 +0100

    Fix training of hypers of GPAR

--- example.py
+++ example.py
@@ -8,6 +8,7 @@ import wbml.plot
 from gpar import GPARRegressor
 from matrix import Dense
 from varz.torch import minimise_l_bfgs_b
+from varz import parametrised, Unbounded
 
 
 def Model(**kw_args):
@@ -17,7 +18,10 @@ def Model(**kw_args):
 # Model parameters:
 p = 16
 m = 4
-gpar = GPARRegressor()
+gpar = GPARRegressor(replace=True, impute=False, noise=0.2,
+                     normalise_y=False, linear=True, nonlinear=False,
+                     linear_scale=1)
+vs = gpar.vs
 
 m_true = m  # True number of latent processes.
 
@@ -35,21 +39,24 @@ inds1_n = B.range(0, 100)
 inds2_n = B.range(100, 200)
 n1 = len(inds1_n)
 n2 = len(inds2_n)
-inds2_p = B.range(8, p)
+inds2_p = B.range(14, p)
 p1 = p
 p2 = len(inds2_p)
+
+# Determine initialisation.
+pcs, vals = B.svd(y.T @ y)[:2]
+h_init = pcs[:, :m] * vals[None, :m] ** .5
 
 
 def _pinv(h):
     return B.cholsolve(B.dense(B.chol(Dense(h.T @ h))), B.dense(h.T))
 
 
-def build(vs):
+@parametrised
+def build(vs, h: Unbounded = h_init):
     """Build model."""
     x_train = torch.tensor(x)
     y_train = Dense(torch.tensor(y))
-
-    h = vs.unbounded(shape=(p, m), name='h')
 
     x_train1 = x_train[inds1_n]
     y_train1 = y_train[inds1_n]
@@ -98,6 +105,7 @@ def nlml(vs):
                     B.sum(model.proj2_orth ** 2))
     reg = 0.5 * (logdet + logfrob)
 
+    gpar.vs = vs
     return -gpar.logpdf(model.x_train, model.proj) + reg
 
 
@@ -107,29 +115,58 @@ def predict(vs, x, num_samples=40):
     h = B.to_numpy(model.h)
 
     # Condition GPAR and sample from the posterior.
+    gpar.vs = vs
     gpar.condition(B.to_numpy(model.x_train), B.to_numpy(model.proj))
     samples = gpar.sample(x, num_samples=num_samples, posterior=True)
+
+    # Compute empirical mean and error bars for predictions of latents.
+    samples_lat = B.stack(*samples, axis=0)
+    mean_lat = B.mean(samples_lat, axis=0)
+    err_lat = 2 * B.std(samples_lat, axis=0)
+
+    # Transform to observation space.
     samples = B.stack(*[sample @ h.T for sample in samples], axis=0)
 
-    # Return empirical mean and error bars.
+    # Compute empirical mean and error bars for predictions in observation
+    # space.
     mean = B.mean(samples, axis=0)
     err = 2 * B.std(samples, axis=0)
-    return mean, mean - err, mean + err
+
+    return ((mean_lat, mean_lat - err_lat, mean_lat + err_lat),
+            (mean, mean - err, mean + err))
 
 
 # Perform training.
-wbml.out.kv('NLML before', nlml(gpar.vs))
-minimise_l_bfgs_b(nlml, gpar.vs, trace=True, iters=400)
-wbml.out.kv('NLML after', nlml(gpar.vs))
+with wbml.out.Section('Before training'):
+    wbml.out.kv('NLML', nlml(vs))
+    vs.print()
+minimise_l_bfgs_b(nlml, vs, trace=True, iters=200)
+with wbml.out.Section('After training'):
+    wbml.out.kv('NLML', nlml(vs))
+    vs.print()
 
 # Perform prediction.
 with wbml.out.Section('Predicting'):
-    mean, lower, upper = predict(gpar.vs, x)
+    ((mean_lat, lower_lat, upper_lat),
+     (mean, lower, upper)) = predict(vs, x)
+
+# Plot predictions for latent processes.
+plt.figure(figsize=(12, 8))
+num = 2
+obs_lat = B.to_numpy(build(vs).proj)
+for i in range(min(m, num * num)):
+    plt.subplot(num, num, i + 1)
+    plt.scatter(x, obs_lat[:, i], label='Observations', c='black')
+    plt.plot(x, mean_lat[:, i], label='Prediction', c='tab:blue')
+    plt.plot(x, lower_lat[:, i], c='tab:blue', ls='--')
+    plt.plot(x, upper_lat[:, i], c='tab:blue', ls='--')
+    wbml.plot.tweak(legend=False)
 
 # Plot predictions.
-plt.figure()
-for i in range(min(p, 16)):
-    plt.subplot(4, 4, i + 1)
+plt.figure(figsize=(12, 8))
+num = 4
+for i in range(min(p, num * num)):
+    plt.subplot(num, num, i + 1)
     plt.scatter(x[inds1_n], y[inds1_n, i], label='Observations', c='black')
     if i in inds2_p:
         plt.scatter(x[inds2_n], y[inds2_n, i], label='Observations', c='black')
@@ -139,4 +176,5 @@ for i in range(min(p, 16)):
     plt.plot(x, lower[:, i], c='tab:blue', ls='--')
     plt.plot(x, upper[:, i], c='tab:blue', ls='--')
     wbml.plot.tweak(legend=False)
+
 plt.show()
